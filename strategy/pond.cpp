340ca390e5f86741aef8fc930cbda7c9d474477c
scrudrv <scrudrv@gmail.com>
2016-01-09 19:34:33 -0500

    file with main method was added

--- strategy/pond.cpp
+++ strategy/pond.cpp
@@ -4,5 +4,25 @@
 
 int main(int argc, char** argv)
 {
+	MallardDuck md;
+	RedHeadDuck rhd;
+	DecoyDuck dd;
+	RubberDuck rd;
 
+	std::cout << "Mallard duck:" << std::endl 
+		<< md.display() << std::endl
+		<< md.quack() << std::endl
+		<< md.fly() << std::endl << std::endl;
+	std::cout << "ReadHead duck:" << std::endl 
+		<< rhd.display() << std::endl
+		<< rhd.quack() << std::endl
+		<< rhd.fly() << std::endl << std::endl;
+	std::cout << "Decoy duck:" << std::endl 
+		<< dd.display() << std::endl
+		<< dd.quack() << std::endl
+		<< dd.fly() << std::endl << std::endl;
+	std::cout << "Rubber duck:" << std::endl 
+		<< rd.display() << std::endl
+		<< rd.quack() << std::endl
+		<< rd.fly() << std::endl << std::endl;
 }
