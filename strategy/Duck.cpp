6d03d466909210c8a4a821d7460dfe454ab50e27
scrudrv <scrudrv@gmail.com>
2016-01-09 19:31:22 -0500

    one more test

--- strategy/Duck.cpp
+++ strategy/Duck.cpp
@@ -1,4 +1,7 @@
 #include "Duck.hpp"
+#include "FlyBehavior.hpp"
+
+#include <iostream>
 
 void Duck::performFly()
 {
@@ -21,4 +24,72 @@ void Duck::setQuack(IQuackBehavior* qb)
 void Duck::display()
 {
 	// nothing to display by default
+}
+
+void MallardDuck::MallardDuck()
+{
+	m_flybehavior = new FlyWithWings();
+	m_quackbehavior = new Quack();
+}
+
+void MallardDuck::~MallardDuck()
+{
+	delete m_flybehavior;
+	delete m_quackbehavior;
+}
+
+void MallardDuck::display()
+{
+	std::cout << "Mallard" << std::endl;
+}
+
+RedHeadDuck::RedHeadDuck()
+{
+	m_flybehavior = new FlyWithWings();
+	m_quackbehavior = new Quack();
+}
+
+RedHeadDuck::~RedHeadDuck()
+{
+	delete m_flybehavior;
+	delete m_quackbehavior;
+}
+
+void RedHeadDuck::display()
+{
+	std::cout << "Red head" << std::endl;
+}
+
+RubberDuck::RubberDuck()
+{
+	m_flybehavior = new FlyNoWay();
+	m_quackbehavior = new Squeak();
+}
+
+RubberDuck::~RubberDuck()
+{
+	delete m_flybehavior;
+	delete m_quackbehavior;
+}
+
+void RubberDuck::display()
+{
+	std::cout << "Rubber" << std::endl;
+}
+
+DecoyDuck::DecoyDuck()
+{
+	m_flybehavior = new FlyNoWay();
+	m_quackbehavior = new MuteQuack();
+}
+
+DecoyDuck::~DecoyDuck()
+{
+	delete m_flybehavior;
+	delete m_quackbehavior;
+}
+
+void DecoyDuck::display()
+{
+	std::cout << "Decoy" << std::endl;
 }
