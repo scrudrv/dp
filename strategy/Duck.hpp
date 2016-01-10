45a0ecb8eb962cd857caf884af73bda88441a881
scrudrv <scrudrv@gmail.com>
2016-01-09 19:32:35 -0500

    3rd test

--- strategy/Duck.hpp
+++ strategy/Duck.hpp
@@ -1,8 +1,12 @@
+
+class IFlyBehavior;
+class IQuackBehavior;
+
 class Duck
 {
 private:
-	IFlyBehavior* flybehavior;
-	IQuackBehavior* quackbehavior;
+	IFlyBehavior* m_flybehavior;
+	IQuackBehavior* m_quackbehavior;
 
 public:
 	void performFly();
@@ -12,4 +16,40 @@ public:
 	void setQuack(IQuackBehavior* qb);
 
 	virtual void display();
+};
+
+class MallardDuck : public Duck
+{
+public:
+	MallardDuck();
+	~MallardDuck();
+
+	void display();
+};
+
+class RedHeadDuck : public Duck
+{
+public:
+	RedHeadDuck();
+	~RedHeadDuck();
+
+	void display();
+};
+
+class RubberDuck : public Duck
+{
+public:
+	RubberDuck();
+	~RubberDuck();
+
+	void display();
+};
+
+class DecoyDuck : public Duck
+{
+public:
+	DecoyDuck();
+	~DecoyDuck();
+
+	void display();
 };
